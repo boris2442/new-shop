3d3599091b7a8b7d7fecf3f1d2cdf4476203dded
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-23 11:30:50 +0100

    text

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const header=document.querySelector("nav");
 const nav=document.querySelector(".navigation");
 const linkNav=document.querySelectorAll(".navigation a");
+const searchContent=document.querySelector(".search-content");
+const btnSearch=document.querySelector("#btn-search");
+const btnBurger=document.querySelector("#burger-menu");
+
+btnBurger.addEventListener("click",()=>{
+    nav.classList.toggle("active")
+})
+
