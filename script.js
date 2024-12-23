d77b5fbf9260c9c26f58879e34d5a611c9de8ef2
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-23 12:02:58 +0100

    text and debug code

--- script.js
+++ script.js
@@ -7,5 +7,26 @@ const btnBurger=document.querySelector("#burger-menu");
 
 btnBurger.addEventListener("click",()=>{
     nav.classList.toggle("active")
+    btnBurger.classList.toggle("bx-x")
+    searchContent.classList.remove("active")
+})
+
+linkNav.forEach(link=>{
+    link.addEventListener("click",()=>{
+   nav.classList.remove("active")
+    btnBurger.classList.remove("bx-x")
+    })
+});
+
+
+btnSearch.addEventListener("click", ()=>{
+    searchContent.classList.toggle("active")
+    nav.classList.remove("active")
+    btnBurger.classList.remove("bx-x")
+});
+
+window.addEventListener("scroll", ()=>{
+    nav.classList.remove("active")
+    btnBurger.classList.remove("bx-x")
 })
 
