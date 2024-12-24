f6d89fe2f7a5eff08e92cbe6e249e7ba319d4429
boris2442 <aubinborissimotsebo@gmail.com>
2024-12-24 10:40:03 +0100

    text

--- script.js
+++ script.js
@@ -69,4 +69,24 @@ const app=new Typewriter (title, {
 
 // partie destinee au swiper library 
 
-
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
